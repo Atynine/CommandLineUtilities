3da4a917c75e47d54bfe175567e82ca6451b7797
Rylan Hoss <tehrhio@live.com>
2016-07-11 23:01:11 -0500

    Re-wrote use the JSON library and be much easier to read
    Also pulls champion names from Riot's API

--- src/Commands/LeagueOfLegends/Champions/UpdateChampions.java
+++ src/Commands/LeagueOfLegends/Champions/UpdateChampions.java
@@ -16,20 +16,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class UpdateChampions extends BasicCommand {
-    private static final String[] championNames = {"Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Ashe", "AurelionSol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Chogath", "Corki", "Darius", "Diana", "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "FiddleSticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar","Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "KhaZix", "Kindred", "KogMaw", "Leblanc", "LeeSin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux","Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "Mordekaiser","Morgana", "Nami", "Nasus","Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani" , "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana","Trundle","Tryndamere","TwistedFate","Twitch","Udyr","Urgot","Varus","Vayne","VelKoz","Vi","Viktor","Vladimir","Volibear","Warwick","MonkeyKing","Xerath","XinZhao","Yasuo","Yorick","Zac","Zed","Ziggs","Zilean","Zyra"};
     private static final String baseChampionURL = "http://champion.gg/champion/";
     private static final int[] UTILITIES = {2003,2043,3363,3364,2139,2140,2138};
 
     public void runCommand(String input){
-        //Load champion names from API
         List<String> championNames = getChampionNames();
         if(championNames.size() == 0) return;
         println("Starting creation of item sets - This could take a few minutes");
         for(String championName : championNames){
             //Load Champion.gg data for this champion
             println("> Creating champion builds for " + championName);
-            String htmlText = readURL(baseChampionURL + championName);
-            if(htmlText.equals("")) continue;
             //Create directory for champion
             File directory = new File("Config/Champions/"+championName+"/Recommended");
             if(directory.mkdirs()){
@@ -42,20 +38,19 @@ public class UpdateChampions extends BasicCommand {
                 currentFile.delete();
             }
             //Get all roles for this champion
-            String[] split = htmlText.split("/champion/"+championName+"/");
-            for(int i = 1; i < split.length; i++){
-                String role = split[i].substring(0,split[i].indexOf("\">"));
-                htmlText = readURL(baseChampionURL + championName + "/" + role);
+            for(String role : getRoles(championName)){
+                String htmlText = readURL(baseChampionURL + championName + "/" + role);
                 println("Generating Most Frequent build for " + role);
                 generateMostFrequent(championName,htmlText,role);
                 println("Generating Highest Win Percent build for " + role);
-                generateHighestWinPercent(championName,htmlText,role);
+                generateHighestWinRate(championName,htmlText,role);
             }
         }
         println("> Finished updating item sets");
     }
 
     private List<String> getChampionNames(){
+        //Load champion names from Riot Games API
         final String API_KEY_PATH = "APIKey.ini";
         final String URL_CHAMPION_DATA = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=";
         try{
@@ -74,68 +69,90 @@ public class UpdateChampions extends BasicCommand {
         }
         return new ArrayList<>();
     }
-    private void generateHighestWinPercent(String championName, String htmlText, String role){
-        //Add header to json file
-        ArrayList<String> lines = new ArrayList<>();
-        lines.add("{\"title\": \""+role+" Highest Win Percent\",\"type\": \"custom\",\"map\": \"any\",\"mode\": \"any\",\"priority\": false,\"sortrank\": 1,");
-        lines.add("\"champion\": \""+championName+"\",");
-        lines.add("\"blocks\": [{");
+    private List<String> getRoles(String championName){
+        String html = readURL(baseChampionURL + championName);
+        ArrayList<String> roles = new ArrayList<>();
+        String[] split = html.split("/champion/"+championName+"/");
+        for(int i = 1; i < split.length; i++) {
+            roles.add(split[i].substring(0, split[i].indexOf("\"")));
+        }
+        return roles;
+    }
+
+    private void generateHighestWinRate(String championName, String htmlText, String role){
+        //Setup root of json file
+        JSONObject root = new JSONObject();
+        root.put("title", role + " Highest WinRate");
+        root.put("champion", championName);
+        root.put("type", "custom");
+        root.put("map", "any");
+        root.put("mode", "any");
+        root.put("priority", false);
+        root.put("sortrank", 1);
+
+        JSONArray itemBlocks = new JSONArray();
+
         //Read html to find most frequent starting items
         String temp = htmlText.substring(htmlText.indexOf("Highest Win % Starters"));
         temp = temp.substring(0, temp.indexOf("<div class=\"build-text"));
-        extractItems(lines,"Starters",temp,3340);
-        lines.add(",{");
-        println("Generated Highest Win Percent starting items");
+        itemBlocks.put(extractItems("Starters",temp,3340));
+        println("Generated Highest WinRate starting items");
         //Read html to find most frequent core build
         temp = htmlText.substring(htmlText.indexOf("Highest Win % Core Build"));
         temp = temp.substring(0, temp.indexOf("<div class=\"build-text"));
-        extractItems(lines,"Core Build",temp);
-        println("Generated Highest Win Percent core build");
-        lines.add(",{");
+        itemBlocks.put(extractItems("Core Build",temp));
+        println("Generated Highest WinRate core build");
         //Add utility items to build
-        println("Adding Utilities to Highest Win Percent build");
-        extractItems(lines,"Utilities","",UTILITIES);
-        lines.add("]}");
+        println("Adding Utilities to Highest WinRate build");
+        itemBlocks.put(extractItems("Utilities","",UTILITIES));
+
+        root.put("blocks", itemBlocks);
         try{
             Path file = Paths.get("Config/Champions/"+championName+"/Recommended/"+role+"HighestWinRate.json");
-            Files.write(file,lines, Charset.forName("UTF-8"));
+            Files.write(file,root.toString().getBytes());
         }catch(IOException e){
             printError("Failed to save item set file");
         }
     }
     private void generateMostFrequent(String championName, String htmlText, String role){
-        //Add header to json file
-        ArrayList<String> lines = new ArrayList<>();
-        lines.add("{\"title\": \""+role+" Most Frequent\",\"type\": \"custom\",\"map\": \"any\",\"mode\": \"any\",\"priority\": false,\"sortrank\": 1,");
-        lines.add("\"champion\": \""+championName+"\",");
-        lines.add("\"blocks\": [{");
+        //Setup root of json file
+        JSONObject root = new JSONObject();
+        root.put("title", role + " Most Frequent");
+        root.put("champion", championName);
+        root.put("type", "custom");
+        root.put("map", "any");
+        root.put("mode", "any");
+        root.put("priority", false);
+        root.put("sortrank", 1);
+
+        JSONArray itemBlocks = new JSONArray();
+
         //Read html to find most frequent starting items
         String temp = htmlText.substring(htmlText.indexOf("Most Frequent Starters"));
         temp = temp.substring(0, temp.indexOf("<div class=\"build-text"));
-        extractItems(lines,"Starters",temp,3340);
-        lines.add(",{");
+        itemBlocks.put(extractItems("Starters",temp,3340));
         println("Generated Most Frequent starting items");
         //Read html to find most frequent core build
         temp = htmlText.substring(htmlText.indexOf("Most Frequent Core Build"));
         temp = temp.substring(0, temp.indexOf("<div class=\"build-text"));
-        extractItems(lines,"Core Build",temp);
+        itemBlocks.put(extractItems("Core Build",temp));
         println("Generated Most Frequent core build");
-        lines.add(",{");
         //Add utility items to build
         println("Adding Utilities to Most Frequent build");
-        extractItems(lines,"Utilities","",UTILITIES);
-        lines.add("]}");
+        itemBlocks.put(extractItems("Utilities","",UTILITIES));
+
+        root.put("blocks", itemBlocks);
         try{
             Path file = Paths.get("Config/Champions/"+championName+"/Recommended/"+role+"MostFrequent.json");
-            Files.write(file,lines, Charset.forName("UTF-8"));
+            Files.write(file,root.toString().getBytes());
         }catch(IOException e){
             printError("Failed to save item set file");
         }
     }
-    private void extractItems(ArrayList<String> list, String title, String html, int... additionalItems){
+    private JSONObject extractItems(String title, String html, int... additionalItems){
         String[] items = html.split(".png");
-        list.add("\"items\": [");
         LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        //Seek and add items to map
         for(int i = 0; i < items.length-1; i++){
             String s = items[i];
             String itemID = s.substring(s.lastIndexOf("/",s.length())).replaceAll("/","");
@@ -145,29 +162,40 @@ public class UpdateChampions extends BasicCommand {
                 map.put(itemID,1);
             }
         }
-        for(int i = 0; i < additionalItems.length; i++){
-            map.put(additionalItems[i]+"",1);
+        //Add additional items to map
+        for(int i : additionalItems){
+            map.put(i+"",1);
         }
+        JSONObject block = new JSONObject();
+        block.put("type", title);
+        block.put("items", createBlocks(map));
+        return block;
+    }
+    private JSONArray createBlocks(LinkedHashMap<String, Integer> map){
+        //Create JSONArray of items from input map
+        JSONArray blocks = new JSONArray();
         Iterator it = map.entrySet().iterator();
         while(it.hasNext()){
             Map.Entry pair = (Map.Entry)it.next();
             String itemID = pair.getKey().toString();
             int count = Integer.parseInt(pair.getValue().toString());
-            addItem(list,itemID,count);
-            it.remove();
-            if(it.hasNext()){
-                list.add(",");
+            if(itemID.equals("2003") || itemID.equals("2010")){
+                //If it is a health potion or biscuit, also add the other
+                blocks.put(createItemBlock("2003",count));
+                blocks.put(createItemBlock("2010",count));
+            }else{
+                blocks.put(createItemBlock(itemID,count));
             }
+            it.remove();
         }
-        list.add("],\"type\": \""+title+"\"}");
+        return blocks;
     }
-    private void addItem(ArrayList<String> items, String itemID, int count){
-        if(itemID.equals("2003") || itemID.equals("2010")){
-            items.add("{\"count\":" + count + "," + "\"id\":\"2003\"},");
-            items.add("{\"count\":" + count + "," + "\"id\":\"2010\"}");
-        }else{
-            items.add("{\"count\":" + count + "," + "\"id\":\"" + itemID + "\"}");
-        }
+    private JSONObject createItemBlock(String itemID, int count){
+        //Create JSONObject of item from input itemID and count
+        JSONObject obj = new JSONObject();
+        obj.put("id", itemID);
+        obj.put("count", count);
+        return obj;
     }
 
 
